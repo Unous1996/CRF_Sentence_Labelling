ea9ffe92f8e450ba31154568f44ec3cf79eae184
Ao Li <aoli1996@berkeley.edu>
2018-12-08 18:24:40 -0600

    Dec8_1824

--- script/test.py
+++ script/test.py
@@ -10,7 +10,7 @@ def readLabeledData(inputFile):
             raw = line.split()
             sentence = []
             for token in raw:
-                parts = token.split('_');
+                parts = token.split('_')
                 sentence.append((parts[0], parts[1]))
             sens.append(sentence) # append this list as an element to the list of sentences
         return sens
@@ -18,12 +18,32 @@ def readLabeledData(inputFile):
         print("Error: unlabeled data file %s does not exist" % inputFile)  # We should really be throwing an exception here, but for simplicity's sake, this will suffice.
         sys.exit() # exit the script
 
+def readUnlabelledData(inputFile):
+    if os.path.isfile(inputFile):
+        file = open(inputFile, "r")  # open the input file in read-only mode
+        sens = [];
+        for line in file:
+            sentence = line.split()  # split the line into a list of words
+            sens.append(sentence)  # append this list as an element to the list of sentences
+        return sens
+    else:
+        print(
+            "Error: unlabeled data file %s ddoes not exist" % inputFile)  # We should really be throwing an exception here, but for simplicity's sake, this will suffice.
+        sys.exit()
+
 if __name__ == "__main__":
+
     inputfile = sys.argv[1]
     ct = CRFTagger()
     labelled_data = readLabeledData(inputFile=inputfile)
     ct.train(labelled_data, 'model.crf.tagger')
-    ct.tag_sents([['dog','is','good'], ['Cat','eat','meat']])
-    gold_sentences = [[('dog','Noun'),('is','Verb'),('good','Adj')] , [('Cat','Noun'),('eat','Verb'), ('meat','Noun')]]
-    print("Result = ", ct.evaluate(gold_sentences))
+
+    testfile = "../test.txt"
+    test_data = readUnlabelledData(inputFile=testfile)
+
+    goldfile = "../gold.txt"
+    gold_data = readLabeledData(inputFile=goldfile)
+    ct.tag_sents(test_data)
+
+    print("Result = ", ct.evaluate(gold_data))
 
